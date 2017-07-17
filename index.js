0ba94bc9fa45c7eac4ca27c93a54072a88635448
Marc Altmann <marcaltmann@posteo.de>
2017-07-17 23:13:30 +0200

    Add fuge configuration

--- index.js
+++
@@ -1,7 +0,0 @@
-const seneca = require('seneca')()
-
-seneca
-  .client({type: 'tcp', pin: 'role:application'})
-  .act('role:application,cmd:version', function(err, result) {
-    console.log(result.version)
-  })
